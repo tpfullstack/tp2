1d8c6178c24ea08d892c2d8f251f6596f6718187
kenza <kenza.djellaoui@univ-rouen.fr>
2024-11-15 15:33:05 +0100

    feat(header) : Ajouter un header pour le site et qui contient button d'ajout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EventListComponent } from './components/event-list/event-list.component'; // Assurez-vous du bon chemin
 import { CreateEventComponent } from './components/create-event/create-event.component';
+import { HeaderComponent } from './components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EventListComponent, CreateEventComponent ], // Ajouter EventListComponent ici
+  imports: [RouterOutlet, EventListComponent, CreateEventComponent, HeaderComponent ], // Ajouter EventListComponent ici
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  navigateToAddEvent(): void {
+    this.router.navigate(['/create-event']);
+  }
+}
