ed727b6d07544e27e1af457202160cbcda8a1267
kenza <kenza.djellaoui@univ-rouen.fr>
2024-11-23 00:52:25 +0100

    Style(AjoutArtist): Modifier le style de la popin d'ajout.

--- src/app/components/create-artist/create-artist.component.ts
+++ src/app/components/create-artist/create-artist.component.ts
@@ -13,15 +13,29 @@ import { ArtistService } from '../../services/artist.service';
 export class CreateArtistComponent {
   artist = {
     label: '',
-    description: '',
   };
 
   constructor(public router: Router, private artistService: ArtistService) {}
 
-  createArtist() {
-    this.artistService.createArtist(this.artist).subscribe((response) => {
-      console.log('Artiste créé', response);
-      this.router.navigate(['/artists']);
-    });
+  // Fonction de création de l'artiste
+  createArtist(): void {
+    if (this.artist.label) {
+      this.artistService.createArtist(this.artist).subscribe(
+        (response) => {
+          console.log('Artiste créé', response);
+          this.router.navigate(['/artists']); // Redirection vers la liste des artistes
+        },
+        (error) => {
+          console.error('Erreur lors de la création de l\'artiste', error);
+        }
+      );
+    } else {
+      console.error('Le nom et la description de l\'artiste sont requis');
+    }
+  }
+
+  // Fonction de fermeture de la modal
+  closeModal(): void {
+    this.router.navigate(['/artists']);
   }
 }
